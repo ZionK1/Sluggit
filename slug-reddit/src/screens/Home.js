7e66e54d51977bc52021284e4324ee5265f9a0f6
Zion Kang <137556676+ZionK1@users.noreply.github.com>
2024-01-20 23:30:03 -0800

    peter sucks d

--- slug-reddit/src/screens/Home.js
+++ slug-reddit/src/screens/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './style.css'; // Import your stylesheet
 import ucscSlugLogo from './ucscslug.png'; // Import the image
 
 const HomePage = () => {
   return (
-    <div class="home">
+    <div className="home">
       <header>
         {/* UCSC Slug logo */}
         <img src={ucscSlugLogo} alt="UCSC Slug Logo" className="ucsc-logo" />
@@ -13,14 +14,15 @@ const HomePage = () => {
 
       <main>
         {/* Content for the Home page goes here */}
-        <div class="welcome">
+        <div className="welcome">
           <h1>Welcome</h1>
           <h2>Choose a category to get started</h2>
         </div>
-        <div class="buttons">
-          <div class="button">Housing</div>
-          <div class="button">Clubs</div>
-          <div class="button">SlugPals</div>
+        <div className="buttons">
+          <Link to="/SlugHousing" className="button">Housing</Link>
+          {/* Add more buttons for other categories */}
+          <Link to="/SlugEvents" className="button">Clubs</Link>
+          <Link to="/SlugPals" className="button">SlugPals</Link>
         </div>
       </main>
 
--- slug-reddit/src/screens/SlugHousing.js
+++ slug-reddit/src/screens/SlugHousing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './style.css'; // Import your stylesheet
 
 const SlugHousing = () => {
   return (
